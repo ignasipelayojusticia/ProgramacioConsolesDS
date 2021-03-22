9bd3817cbe450f37e4c383bfebfa73c33d94aace
Ignasi Pelayo <ignasi.pelayo@enti.cat>
2021-03-22 19:59:02 +0100

    basic func created

--- mole_killer/source/main.cpp
+++ mole_killer/source/main.cpp
@@ -1,33 +1,56 @@
 #include <nds.h>
 #include <stdio.h>
 
+touchPosition touch;
+bool playing;
+
+void init();
+void quit();
+void step();
+
 int main(void) 
 {
-	touchPosition touch;
+	init();
 
-	consoleDemoInit(); 
-
-	int area = 0;
-
-	while(1) 
+	while(playing) 
 	{
-		scanKeys();
-		int held = keysHeld();
-
-		if(held & KEY_START) break;
-
-		if(held & KEY_TOUCH)
-		{
-			touchRead(&touch);
-
-			iprintf("\x1b[10;0Hx = %04i - %04i\n", touch.rawx, touch.px);
-			iprintf("y = %04i - %04i\n", touch.rawy, touch.py);
-
-
-		}
-
-		swiWaitForVBlank();
+		step();
 	}
 
+	quit();
+
 	return 0;
+}
+
+void init()
+{
+	consoleDemoInit(); 
+	playing = true;
+}
+
+void quit()
+{
+
+}
+
+void step()
+{
+	scanKeys();
+	int held = keysHeld();
+
+	if(held & KEY_START)
+	{
+		playing = false;
+		return;
+	}
+
+	if(held & KEY_TOUCH)
+	{
+		touchRead(&touch);
+
+		iprintf("\x1b[10;0Hx = %04i - %04i\n", touch.rawx, touch.px);
+		iprintf("y = %04i - %04i\n", touch.rawy, touch.py);
+	}
+
+	swiWaitForVBlank();
 }
