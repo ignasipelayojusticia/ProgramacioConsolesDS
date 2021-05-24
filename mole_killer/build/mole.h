787cbfb8dda9db44b8c01385bcd191e9adf465a0
Ignasi Pelayo <ignasi.pelayo@enti.cat>
2021-05-24 15:53:01 +0200

    Last commit

--- mole_killer/build/mole.h
+++ mole_killer/build/mole.h
@@ -8,7 +8,7 @@
 //	+ 32 tiles Metatiled by 4x4 not compressed
 //	Total size: 512 + 2048 = 2560
 //
-//	Time-stamp: 2021-05-17, 19:16:13
+//	Time-stamp: 2021-05-24, 15:51:13
 //	Exported by Cearn's GBA Image Transmogrifier, v0.8.15
 //	( http://www.coranac.com/projects/#grit )
 //
